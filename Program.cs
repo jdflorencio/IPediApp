edf3c16278726a69cf122700f4eff1b35c157765
João Diego <jdflorencio@outlook.com>
2023-03-25 16:27:26 -0300

    minha rota para adicionar categories

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+﻿using IPediApp.Endpoint.Categories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,29 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
