b2560b88170ec843c254e49eda1db171449f84ae
João Diego <jdflorencio@outlook.com>
2023-03-28 00:13:23 -0300

    correção...

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -4,7 +4,7 @@ public class Product : Entity
 {
     //guid permite que geramos o id da forma que queremos
     public string Name { get; set; }
-    public int CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public Category Category { get; set; }
     public string Description{ get; set; }
     public bool hasStock { get; set; }
